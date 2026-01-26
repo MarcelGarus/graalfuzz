4336128098c781db6a1fdf93a2fe3c6dea556315
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2026-01-26 10:01:29 +0100

    fix: maxGroups cant be 0 for ALL

--- src/main/java/de/hpi/swa/analysis/query/GroupLimitSpec.java
+++ src/main/java/de/hpi/swa/analysis/query/GroupLimitSpec.java
@@ -1,10 +1,9 @@
 package de.hpi.swa.analysis.query;
 
-
 public sealed interface GroupLimitSpec {
 
-    public record None() implements GroupLimitSpec {}
-
+    public record None() implements GroupLimitSpec {
+    }
 
     public record LeafsOnly(int maxGroups) implements GroupLimitSpec {
         public LeafsOnly {
@@ -16,7 +15,7 @@ public sealed interface GroupLimitSpec {
 
     public record All(int maxGroups) implements GroupLimitSpec {
         public All {
-            if (maxGroups == 0 || maxGroups < -1) {
+            if (maxGroups < -1) {
                 throw new IllegalArgumentException("maxGroups must be positive or -1 for unlimited");
             }
         }
